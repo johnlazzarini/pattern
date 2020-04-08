2db702455252fd65a7dca33600a28084d831708e
John Lazzarini <jlazzari@mail.sfsu.edu>
2020-04-07 23:03:27 -0700

    added uniform rings

--- s1v3/canvas.js
+++ s1v3/canvas.js
@@ -23,22 +23,30 @@ function Circle(angle, sign, radius, rotationRadius, initialX, initialY) {
 
 Circle.prototype.update = function () {
 
+
+  if (circles[0].radius > (canvasWidth / 1.5)){
+    circles.shift();
+    createCircle();
+  }
+
   this.angle += this.incrementer;
   this.radius += this.incrementer;
 	
-	this.currentX = this.initialX + this.rotationRadius * Math.cos(this.angle);
-	this.currentY = this.initialY + this.rotationRadius * Math.sin(this.angle);
+	this.currentX = this.initialX;
+	this.currentY = this.initialY;
 	
 	if (this.angle >= (Math.PI * 2)) {
 		this.angle = 0;
-    this.incrementer = .01 + Math.random() * .1; //random incrementor
+    //this.incrementer = .01 + Math.random() * .1; //random incrementor
+    this.incrementer = .01; //random incrementor
+
 	}
 
 	// The following code is responsible for actually drawing the circle on the screen
 	mainContext.beginPath();
 	mainContext.arc(this.currentX, this.currentY, this.radius, 0, Math.PI * 2, false);
 	mainContext.closePath();
-	mainContext.fillStyle = 'rgba(177, 0, 129, .1)';
+	mainContext.fillStyle = 'rgba(3, 0, 129, .1)';
 	mainContext.stroke();
 };
 
@@ -75,8 +83,10 @@ function createCircles() {
 
 function createCircle() {
 //create a single circle
+
+console.log(circles.length);
       console.log("hello");
-      let radius = 5 + Math.random() * 40;
+      let radius = Math.random() * 40;
       let initialX = canvasWidth / 2;
       let initialY = canvasHeight / 2;
       let rotationRadius = 1 + Math.random() * 30;
@@ -96,6 +106,7 @@ function createCircle() {
       let circle = new Circle(angle, sign, radius, rotationRadius, initialX, initialY);
       circles.push(circle);
     
+
     // call the draw function approximately 60 times a second
     requestAnimationFrame(draw);
   }
@@ -105,7 +116,7 @@ createCircles();
 function draw() {
 	mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
 	mainContext.fillStyle = '#ffffff';
-	mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+  mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
 	
 	for (let i = 0; i < circles.length; i++) {
 		let circle = circles[i];
